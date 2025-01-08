c98663f67936979800571375473fdc473632df2e
Richard <rhshadowgreen@gmail.com>
2025-01-07 21:04:14 -0800

    Update make.js

--- make.js
+++ make.js
@@ -5,7 +5,7 @@ module.exports = GITHUB_WORKSPACE => {
   console.log(`${GITHUB_WORKSPACE}/posts`);
   
   const posts = [];
-  fs.readdir(GITHUB_WORKSPACE, (err, files) => {
+  fs.readdir(`${GITHUB_WORKSPACE}/posts`, (err, files) => {
     console.log(files);
     files.forEach(file => posts.push(file));
   });

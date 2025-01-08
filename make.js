46de8b56a7ae51d3198919010efbebe80e2c97e0
Richard <rhshadowgreen@gmail.com>
2025-01-07 20:59:30 -0800

    Update make.js

--- make.js
+++ make.js
@@ -1,11 +1,15 @@
 const fs = require('fs')
 
 module.exports = GITHUB_WORKSPACE => {
+
+  console.log(`${GITHUB_WORKSPACE}/posts`)
   
   const posts = [];
   fs.readdir(`${GITHUB_WORKSPACE}/posts`, (err, files) => {
     files.forEach(file => posts.push(file));
   });
+
+  console.log(posts)
   
   fs.writeFileSync("posts.js", 
     `export function posts() {

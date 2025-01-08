aaa0d8ed9f9721bb289fe6b3c917cf76f2c885b3
Richard <rhshadowgreen@gmail.com>
2025-01-07 19:07:49 -0800

    Create dir.js

---
+++ dir.js
@@ -0,0 +1,16 @@
+export function dir() {
+  return [
+    {
+      type: 'file',
+      data: 5
+    },
+    {
+      type: 'dir',
+      data: 6
+    },
+    {
+      type: 'file',
+      data: 7
+    }
+  ];
+}

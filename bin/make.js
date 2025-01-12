0967b24d83a40b24e7ca15992bc8180167f83ce8
RichardHayes <rhshadowgreen@gmail.com>
2025-01-11 18:19:21 -0800

    organization of files and imports

--- bin/make.js
+++ bin/make.js
@@ -1,13 +1,11 @@
 const fs = require('fs')
 
 module.exports = GITHUB_WORKSPACE => {
-
-  //
   const posts = [];
   const files = fs.readdirSync(`${GITHUB_WORKSPACE}/posts`);
   files.forEach(file => posts.push(file));
 
-  fs.writeFileSync("_posts.js", 
+  fs.writeFileSync("_posts.js",
     `export function posts() {
       return ${JSON.stringify(posts, null, 2)};
     }`
--- paper.js
+++ paper.js
@@ -1,4 +1,11 @@
+import { posts as postData } from './_posts.js';
+import showdown from './lib/showdown.js';
+
 export function html(markdownStrs) {
     const converter = new showdown.Converter();
     document.body.innerHTML = converter.makeHtml(markdownStrs.join(""));
+}
+
+export function posts() {
+    return postData();
 }

63984e9a3c6729c33ba6543060df1fdcb606d2aa
RichardHayes <rhshadowgreen@gmail.com>
2025-01-12 17:30:15 -0800

    fix

--- paper.js
+++ paper.js
@@ -13,24 +13,25 @@ function parseMatter(frontmatterLines) {
   }
 
 export function html(markdown) {
-    const create = n => document.createElement(n);
+  markdown = markdown.join("");
+  const create = n => document.createElement(n);
 
-    // Split frontmatter and markdown content
-    const split = markdown.split("+++");
-    const frontmatter = parseMatter(split[1]);
-    const content = split[2];
+  // Split frontmatter and markdown content
+  const split = markdown.split("+++");
+  const frontmatter = parseMatter(split[1]);
+  const content = split[2];
 
-    // Add back link to body
-    const a = create("a");
-    a.setAttribute("href", "../index.html");
-    a.innerHTML = "< back";
-    document.body.appendChild(a);
+  // Add back link to body
+  const a = create("a");
+  a.setAttribute("href", "../index.html");
+  a.innerHTML = "< back";
+  document.body.appendChild(a);
 
-    // Create div to hold post content
-    const div = create("div");
-    document.body.appendChild(div);
+  // Create div to hold post content
+  const div = create("div");
+  document.body.appendChild(div);
 
-    // Add post content to body
-    const converter = new showdown.Converter();
-    div.innerHTML = converter.makeHtml(content);
+  // Add post content to body
+  const converter = new showdown.Converter();
+  div.innerHTML = converter.makeHtml(content);
 }
